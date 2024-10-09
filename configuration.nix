713d99f4f43ba975c317a3eaa9a09e872b48cdb4
Ray <rayantonius@gmail.com>
2024-10-09 10:04:51 +0700

    remove hyprland

--- configuration.nix
+++ configuration.nix
@@ -85,7 +85,6 @@
 
   # Install firefox.
   programs.firefox.enable = true;
-  programs.hyprland.enable = true;
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
